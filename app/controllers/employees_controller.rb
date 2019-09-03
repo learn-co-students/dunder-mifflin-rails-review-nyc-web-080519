11a28b2ef246e4f45d987a383696996f0b3e912d
DanBehrman <dan.behrman@wework.com>
2019-09-03 17:05:34 -0400

    finished

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            flash.now[:messages] = @employee.errors.full_messages[0]
+            render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        if @employee.update(employee_params)
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            flash.now[:messages] = @employee.errors.full_messages[0]
+            render :edit
+        end
+    end
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,12 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :office, presence: true
+    validates :alias, uniqueness: true
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
 end
