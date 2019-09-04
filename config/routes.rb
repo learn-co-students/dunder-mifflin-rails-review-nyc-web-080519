5bb049dc4fa86407a942ce0aff9dce02be12f268
kgriswo1 <kgriswo1@binghamton.edu>
2019-09-04 09:22:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Dog routes
+  get '/dogs', to: 'dogs#index', as: 'dogs'
+  get '/dogs/:id', to: 'dogs#show', as: 'dog'
+
+  #Employee routes
+  get '/employees', to: 'employees#index', as: 'employees'
+  get '/employees/new', to: 'employees#new', as: 'new_employee'
+  get '/employees/:id', to: 'employees#show', as: 'employee'
+  get '/employees/:id/edit', to: 'employees#edit', as: 'edit_employee'
+  post '/employees', to: 'employees#create'
+  patch 'employees/:id', to: 'employees#update'
+  delete 'employees/:id', to: 'employees#destroy', as: 'delete_employee'
 end
