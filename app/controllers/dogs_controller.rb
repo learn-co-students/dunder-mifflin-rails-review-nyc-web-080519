df2a2bd2ac8f334be95d50f9fa736103d9fde5ae
Rutger <rutgerjmckenna@gmail.com>
2019-09-04 09:19:33 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,57 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+        @employees = Employee.all
+        if @dog.valid?
+            @dog.save
+            redirect_to dog_path(@dog)
+          else
+            flash.now[:messages] = @dog.errors.full_messages[0]
+            @employees = Employee.all
+            render :new
+          end
+    end
+
+    def create
+        @dog = Dog.create(params[:id])
+        if @dog.valid?
+            @dog.save
+            redirect_to dog_path(@dog)
+          else
+            flash.now[:messages] = @dog.errors.full_messages[0]
+            @employees = Employee.all
+            render :new
+          end
+    end
+
+    def update
+        @dog = Dog.find(params[:id])
+        @dog.update(dog_params)
+        redirect_to dog_path(dog)
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect_to dogs_path
+    end
+
+    #----------------------PARAMS-------------------------------
+    def dog_params
+        params.require[:dog].permit[:name, :breed, :age]
+    end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,56 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        @dog = @employee.dog
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.new(params[:id])
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+          else
+            flash.now[:messages] = @employee.errors.full_messages[0]
+            @dogs = Dog.all
+            render :new
+          end
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+        if @employee.update(employee_params)
+            @employee.update(employee_params)
+            redirect_to employee_path(@employee)
+          else
+            flash.now[:messages] = @employee.errors.full_messages[0]
+            @dogs = Dog.all
+            render :edit
+        end
+    end
+
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy
+        redirect_to employees_path
+    end
+
+    #----------------------PARAMS-------------------------------
+    def employee_params
+        params.require(:employee).permit(:fist_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
 end
